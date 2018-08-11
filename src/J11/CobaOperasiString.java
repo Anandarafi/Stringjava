b399835ec07efde79a71a1e3f9f86a9cda47bece
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-08-11 19:31:35 +0700

    Initial Commit

---
+++ src/J11/CobaOperasiString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author Anandarafi
+ */
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+        String identitas = ("Ananda Rafi Amanullah / X RPL 3 / 04");
+        System.out.println("Identitas : " + identitas);
+        
+        
+    }
+}
