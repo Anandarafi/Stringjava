dc72f14c70b4d8db535e8f7b1853aa71d5ec2241
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-08-14 06:18:01 +0700

    c oba method format

--- src/J11/Praktikum1.java
+++ src/J11/Praktikum1.java
@@ -5,6 +5,8 @@
  */
 package J11;
 
+import java.util.Locale;
+
 /**
  *
  * @author Anandarafi
@@ -72,7 +74,11 @@ public class Praktikum1 {
         System.out.println("pecah berdasarkan huruf 'r' di variabel tclean : ");
         for(int i = 0; i < arrTr.length; i++)
             System.out.println(arrTr[i]);
-       
+       double d =1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d ));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));
+ 
     
     }
 }
