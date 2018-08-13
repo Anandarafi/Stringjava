d373f02fe84e9ae4a05bbdc17ad1356c53af35d2
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-08-14 06:13:15 +0700

    initial commit

---
+++ src/J11/Praktikum1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author Anandarafi
+ */
+public class Praktikum1 {
+    public static void main(String[] args)
+    {
+        String identitas = ("Ananda Rafi Amanullah / X RPL 3 / 04");
+        System.out.println("Identitas : " + identitas);
+        
+       
+    
+    }
+}
