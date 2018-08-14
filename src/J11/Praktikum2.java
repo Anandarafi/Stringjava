345a6c2ca46fc3f5e0ca9ac32fee2d098466abe1
Anandarafi <Anandarafi@DESKTOP-OQL60SH.mshome.net>
2018-08-14 08:09:19 +0700

    Initial commit

---
+++ src/J11/Praktikum2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author Anandarafi
+ */
+public class Praktikum2 {
+     private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+     public static void main(String[] args)
+    {
+        String identitas = ("Ananda Rafi Amanullah / X RPL 3 / 04");
+        tampilJudul(identitas);
+    }
+}
